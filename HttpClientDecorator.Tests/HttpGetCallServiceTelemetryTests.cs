880989e12c173407ff117d605e712d9dc5492b0a
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-04-20 09:37:17 -0500

    Add Moq for Unit Tests Add Unit Tests for Services Update Home Page

---
+++ HttpClientDecorator.Tests/HttpGetCallServiceTelemetryTests.cs
@@ -0,0 +1,72 @@
+﻿using HttpClientDecorator.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace HttpClientDecorator.Tests
+{
+    [TestClass]
+    public class HttpGetCallServiceTelemetryTests
+    {
+        private Mock<IHttpGetCallService> _mockService;
+        private Mock<ILogger<HttpGetCallServiceTelemetry>> _mockLogger;
+        private HttpGetCallServiceTelemetry _telemetryService;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _mockService = new Mock<IHttpGetCallService>();
+            _mockLogger = new Mock<ILogger<HttpGetCallServiceTelemetry>>();
+            _telemetryService = new HttpGetCallServiceTelemetry(_mockLogger.Object, _mockService.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsExpectedResult()
+        {
+            // Arrange
+            var expectedResponse = new HttpGetCallResults<string>
+            {
+                RequestPath = "https://example.com",
+                ResponseResults = "OK",
+                Retries = 0
+            };
+            _mockService.Setup(x => x.GetAsync(It.IsAny<HttpGetCallResults<string>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = await _telemetryService.GetAsync(expectedResponse, CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(expectedResponse.RequestPath, result.RequestPath);
+            Assert.AreEqual(expectedResponse.ResponseResults, result.ResponseResults);
+            Assert.AreEqual(expectedResponse.Retries, result.Retries);
+            Assert.IsNotNull(result.CompletionDate);
+            Assert.IsTrue(result.ElapsedMilliseconds >= 0);
+            _mockLogger.Verify(x => x.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<Exception>(), (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Never);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_LogsErrorAndReturnsOriginalResult_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var expectedResponse = new HttpGetCallResults<string>
+            {
+                RequestPath = "https://example.com",
+                ResponseResults = "OK",
+                Retries = 0
+            };
+            var expectedException = new Exception("Something went wrong");
+            _mockService.Setup(x => x.GetAsync(It.IsAny<HttpGetCallResults<string>>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(expectedException);
+
+            // Act
+            var result = await _telemetryService.GetAsync(expectedResponse, CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(expectedResponse.RequestPath, result.RequestPath);
+            Assert.IsNull(result.ResponseResults);
+            Assert.AreEqual(expectedResponse.Retries, result.Retries);
+            Assert.IsNotNull(result.CompletionDate);
+            Assert.IsTrue(result.ElapsedMilliseconds >= 0);
+        }
+    }
+}
---
+++ HttpClientDecorator.Tests/HttpGetCallServiceTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System.Net;
+using System.Text.Json;
+
+namespace HttpClientDecorator.Tests
+{
+    [TestClass]
+    public class HttpGetCallServiceTests
+    {
+        private HttpGetCallService _httpGetCallService;
+        private Mock<ILogger<HttpGetCallService>> _loggerMock;
+        private Mock<IHttpClientFactory> _httpClientFactoryMock;
+        private Mock<HttpClient> _httpClientMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<HttpGetCallService>>();
+            _httpClientMock = new Mock<HttpClient>();
+            _httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            _httpClientFactoryMock.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(_httpClientMock.Object);
+            _httpGetCallService = new HttpGetCallService(_loggerMock.Object, _httpClientFactoryMock.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_NullGetCallResults_ThrowsArgumentNullException()
+        {
+            // Arrange
+            HttpGetCallResults<object> getCallResults = null;
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(() => _httpGetCallService.GetAsync(getCallResults, CancellationToken.None));
+        }
+
+        [TestMethod]
+        public async Task GetAsync_EmptyRequestPath_ThrowsArgumentException()
+        {
+            // Arrange
+            var getCallResults = new HttpGetCallResults<object>
+            {
+                RequestPath = string.Empty
+            };
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() => _httpGetCallService.GetAsync(getCallResults, CancellationToken.None));
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Success_ReturnsHttpGetCallResultsWithResponseData()
+        {
+            // Arrange
+            var getCallResults = new HttpGetCallResults<string>
+            {
+                RequestPath = "http://example.com",
+                ResponseResults = "success"
+            };
+            var httpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(JsonSerializer.Serialize(getCallResults.ResponseResults))
+            };
+            _httpClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(httpResponseMessage);
+
+            // Act
+            var result = await _httpGetCallService.GetAsync(getCallResults, CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(getCallResults.ResponseResults, result.ResponseResults);
+            Assert.IsNull(result.ErrorMessage);
+            Assert.AreEqual(0, result.Retries);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_DeserializeException_LogsCriticalErrorAndReturnsHttpGetCallResultsWithError()
+        {
+            // Arrange
+            var getCallResults = new HttpGetCallResults<string>
+            {
+                RequestPath = "http://example.com",
+                ResponseResults = "invalid-json"
+            };
+            var httpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(getCallResults.ResponseResults)
+            };
+            _httpClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(httpResponseMessage);
+
+            // Act
+            var result = await _httpGetCallService.GetAsync(getCallResults, CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(result.ResponseResults, "invalid-json");
+            Assert.IsNotNull(result.ErrorMessage);
+            Assert.IsTrue(result.ErrorMessage.StartsWith("HttpGetCallService:GetAsync:DeserializeException"));
+        }
+    }
+}
