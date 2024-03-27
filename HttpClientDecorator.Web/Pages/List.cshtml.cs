dc2f1d9835a7f8a4598a81c3669e160921522c49
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-03-27 14:35:28 -0500

    Add config value for RequetURL

--- HttpClientDecorator.Web/Pages/List.cshtml.cs
+++ HttpClientDecorator.Web/Pages/List.cshtml.cs
@@ -6,18 +6,21 @@ public class ListModel : PageModel
 {
     private readonly ILogger<ListModel> _logger;
     private readonly IHttpClientService _service;
+    private readonly IConfiguration _configuration;
 
-    public ListModel(ILogger<ListModel> logger, IHttpClientService getCallService)
+    public ListModel(ILogger<ListModel> logger, IHttpClientService getCallService, IConfiguration configuration)
     {
         _logger = logger;
         _service = getCallService;
+        _configuration = configuration;
     }
 
     public IList<HttpClientSendRequest<SiteStatus>> HttpGetCallResults { get; set; } = default!;
 
     public async Task OnGetAsync(CancellationToken ct = default)
     {
-        var taskProcessor = new HttpClientConcurrentProcessor(taskId => new HttpClientConcurrentModel(taskId, "https://asyncdemoweb.azurewebsites.net/status"), _service);
+        string requestUrl = _configuration.GetValue<string>("RequestUrl") ?? "https://asyncdemoweb.azurewebsites.net/status";
+        var taskProcessor = new HttpClientConcurrentProcessor(taskId => new HttpClientConcurrentModel(taskId, requestUrl), _service);
         List<HttpClientConcurrentModel> results = await taskProcessor.RunAsync(100, 10, ct);
         HttpGetCallResults = results.Select(s => s.statusCall).ToList();
     }
